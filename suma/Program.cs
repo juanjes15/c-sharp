03d3fbb58116d9b233998aa13dd044938296750d
juanjes15 <juanjes15@gmail.com>
2022-05-06 21:37:59 -0500

    main

---
+++ suma/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = 0, num2 = 0, num3 = 0, total = 0;
+
+Console.WriteLine("Ingrese el primer número");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo número");
+num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el tercer número");
+num3 = int.Parse(Console.ReadLine());
+
+total = num1 + num2 + num3;
+
+Console.WriteLine(total);
