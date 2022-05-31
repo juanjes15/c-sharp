46b9ff5d260dea1ec8d227b75ff7a0272f9abbce
juanjes15 <105091152+juanjes15@users.noreply.github.com>
2022-05-31 11:48:36 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,21 @@
-﻿int[] n = new int[4];
-string[] b = new string[4];
-char clase = 'N';
-int temp;
+﻿float n1, n2, n3, n4, n5, res;
 
-for (int i = 0; i < n.Length; i++)
+for (int i = 0; i < 5; i++)
 {
-    Console.WriteLine("Ingrese el número " + (i + 1) + " de la IP");
-    n[i] = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese una nota");
+    n(i) = float.Parse(Console.ReadLine());
 }
 
-if ((n[0] >= 1 && n[0] <= 223) && (n[1] >= 0 && n[1] <= 255) && (n[2] >= 0 && n[2] <= 255) && (n[3] >= 0 && n[3] <= 255))
+for (int i = 1; i < 6; i++)
 {
-    if (n[0] <= 127)
+    if (i < 3)
     {
-        clase = 'A';
+        res = res + (ni * 0, 125);
     }
-    else if (n[0] <= 191)
+    else
     {
-        clase = 'B';
-    }
-    else if (n[0] <= 223)
-    {
-        clase = 'C';
-    }
-
-    for (int i = 0; i < b.Length; i++)
-    {
-        temp = n[i];
-        for (int j = 0; j < 8; j++)
-        {
-            b[i] = string.Format("{0}{1}", temp % 2, b[i]);
-            temp = temp / 2;
-        }
+        res = res + (ni * 0, 25);
     }
 }
-else
-{
-    Console.WriteLine("Número invalido para una IP");
-}
 
-Console.WriteLine("La IP es: " + n[0] + "." + n[1] + "." + n[2] + "." + n[3]);
-Console.WriteLine(string.Format("La IP en binario es: {0}.{1}.{2}.{3}", b[0], b[1], b[2], b[3]));
-Console.WriteLine($"Su clasificación es: {clase}");
+Console.WrileLine("La nota final es: " + res);
